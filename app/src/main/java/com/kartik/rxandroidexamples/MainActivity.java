a12a3167098e29c89a0a733f3ed8309858f4ea26
kartik.gujarati <kartik.gujarati@malauzai.com>
2017-09-06 20:50:17 -0500

    Initial commit

---
+++ app/src/main/java/com/kartik/rxandroidexamples/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kartik.rxandroidexamples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+/* -------------------------------------- Lifecycle Methods -------------------------------------- */
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+/* ------------------------------------ End Lifecycle Methods ------------------------------------ */
+}
